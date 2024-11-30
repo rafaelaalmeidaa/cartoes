6d702b078713be9ccc85b09811b8e8305fe0c5f5
rafaela <00001101565524sp@al.educacao.sp.gov.br>
2024-11-30 12:22:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Parques',
+    'Qual foi o primeiro parque?',
+    'Disneyland Park, em Anaheim (Estados Unidos).'
+)
+
+criaCartao(
+    'Criação',
+    'Quem criou a Disney?',
+    'Walt Disney, ele fundou o maior estúdio de animação de Hollywood e criou parques temáticos da Disney.'
+)
+
+criaCartao(
+    'Animação',
+    'Qual foi a primeira animação?',
+    'Branca de Neve e os Sete Anões, lançado em 1937.'
+)
+
+criaCartao(
+    'Mickey',
+    'Como surgiram o Mickey e Minnie?',
+    'o surgimento da Minnie Mouse tem uma ligação direta com a do Mickey, onde a história é desenrolada no documentário, Mickey: A História de um Camundongo.'
+)
+
+criaCartao(
+    'live action',
+    'Qual foi o primeiro live action da Disney? ',
+    'O livro da selva.'
+)
+
+criaCartao(
+    'Castelo',
+    'De qual princesa é o castelo que aparece na logo da Disney?',
+    'Cinderela'
+) 
